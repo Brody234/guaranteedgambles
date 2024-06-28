0bc82055a2049ba39d0bd4ee571a9a6363abb382
Brody234 <brodymassad@gmail.com>
2024-06-28 18:56:37 -0400

    commit
    commit

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style = {{width: '100%', height: '100vh', alignContent: "center", alignItems: "center", textAlign: "center"}}>
+          <p style = {{fontSize: '60px'}}>Under Construction</p>
+    </div>
+  );
+}
