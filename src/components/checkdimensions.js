0113890fc80622232d3445609c005f29fdf3d93b
Brody234 <brodymassad@gmail.com>
2024-07-08 23:15:15 -0400

    please

--- src/components/checkdimensions.js
+++ src/components/checkdimensions.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const CheckDimensions = ({ setIsHeightLessThanWidth }) => {
     useEffect(() => {
+        if(typeof window !== 'undefined'){
         const checkDimensions = () => {
             const { innerHeight, innerWidth } = window;
             setIsHeightLessThanWidth(innerHeight < innerWidth);
@@ -13,6 +14,7 @@ const CheckDimensions = ({ setIsHeightLessThanWidth }) => {
         return () => {
             window.removeEventListener('resize', checkDimensions);
         };
+    }
     }, [setIsHeightLessThanWidth]);
 
     return null;
