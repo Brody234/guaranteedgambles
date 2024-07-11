658f855fa73c52055a1bd9426e32c2a9e05ac67b
Brody234 <brodymassad@gmail.com>
2024-07-11 19:12:00 -0400

    test commit

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,13 +1,25 @@
 import Head from 'next/head';
+import Script from 'next/script';
 
 const CustomHead = () => (
-  <Head>
-    <title>Guaranteed Gambles</title>
-    <meta name="description" content="The best site for finding arbitrage trades." />
-    <link rel="icon" href="/favicon.ico" />
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"  rel="stylesheet" />
+  <>
+    <Head>
+      <title>Guaranteed Gambles</title>
+      <meta name="description" content="The best site for finding arbitrage trades." />
+      <link rel="icon" href="/favicon.ico" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16641151078" strategy="afterInteractive"></Script>
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-  </Head>
+        gtag('config', 'AW-16641151078');
+      `}
+    </Script>
+  </>
 );
 
 export default CustomHead;
