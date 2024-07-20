3b9918f857cf1bf7f125c140e4c0819ad24e33d2
Brody234 <brodymassad@gmail.com>
2024-07-20 14:36:32 -0400

    Partner TOS Page Fix

--- src/app/partnertos/page.jsx
+++ src/app/partnertos/page.jsx
@@ -10,7 +10,7 @@ return(
         <button className="primary-button" onClick = {()=>{router.back()}}><p>Go Back</p></button>
             <h1  id="h.uv5xunuva0hf">
                 <span >
-                    Affiliate Terms of Service
+                    Partner Terms of Service
                 </span>
             </h1>
             <h2  id="h.8bvi4mm58gdz">
@@ -19,13 +19,13 @@ return(
                 </span>
             </h2>
             <p >
-                <span >Welcome to the Affiliate Program of Guaranteed Gambles, operated by Gamer Coach LLC. By joining our program and promoting our products/services, you agree to abide by the following terms and conditions. Please read them carefully.</span>
+                <span >Welcome to the Partner Program of Guaranteed Gambles, operated by Gamer Coach LLC. By joining our program and promoting our products/services, you agree to abide by the following terms and conditions. Please read them carefully.</span>
             </p>
             <h2  id="h.s7avgy9e48m7">
                 <span >Commission Structure</span>
             </h2>
             <p >
-                <span >As an affiliate, you will earn a commission based on a percentage of the sales generated through your unique affiliate link. The commission rate is specified at the time of your registration. For example, if your commission rate is 50%, you will receive 50% of the total sales amount from customers referred by you.</span>
+                <span >As an partner, you will earn a commission based on a percentage of the sales generated through your unique partner link. The commission rate is specified at the time of your registration. For example, if your commission rate is 50%, you will receive 50% of the total sales amount from customers referred by you.</span>
             </p>
             <h2  id="h.liz1k9j2lql6">
                 <span >Refund Policy</span>
@@ -37,14 +37,14 @@ return(
                 <span >Approval and Payouts</span>
             </h2>
             <p >
-                <span >There is a 30-day wait period for sales to be approved. After this period, you can request a payout to your PayPal account. You may request other forms of payout; however, depending on the form you request, we may ask you to find another form.</span>
+                <span >There is a 30-day wait period for sales to be approved. After this period, you can request a payout to your PayPal account. You may request other forms of payout; however, depending on the form you request, we may ask you to find another form. This wait period is set up to give us time to avoid charge backs and scams.</span>
             </p>
             <h2  id="h.8nxx2qghgtg2">
                 <span >Tax Forms</span>
             </h2>
             <p >
                 <span >
-                    Prior to receiving any payouts, you will be required to submit the necessary tax forms to us. These requirements may change depending on where you live and the total value of commissions you are receiving. It is your responsibility to ensure that the tax forms are accurate and submitted on time.
+                    Prior to receiving any payouts, you will be required to submit the necessary tax forms to us. These requirements may change depending on where you live and the total value of commissions you are receiving. It is your responsibility to ensure that the tax forms are accurate and submitted on time. Delayed or inaccurate tax forms can result in delayed payouts. This is required as we need to write off commissions in taxes or else between commissions and taxes we could lose money.
                 </span>
             </p>
             <h2  id="h.55qoyf5p394j">
@@ -64,7 +64,7 @@ return(
             </p>
             <p >
                 <span >
-                    Thank you for being a part of our affiliate program.
+                    Thank you for being a part of our partner program.
                 </span>
             </p>
             <p >
